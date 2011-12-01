8b1108f4fcff30ba13ecc1395247f68f4984c39c
Winston Prakash <winston.prakash@gmail.com>
2011-12-01 14:17:42 -0800

    Change the license header as suggested in Eclipse bug id 365319

--- src/main/java/org/eclipse/hudson/scm/subversion/SubversionEventHandlerImpl.java
+++ src/main/java/org/eclipse/hudson/scm/subversion/SubversionEventHandlerImpl.java
@@ -1,17 +1,8 @@
 /*******************************************************************************
- *
- * Copyright (c) 2004-2007 TMate Software Ltd.
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *
- * Kohsuke Kawaguchi
- *
+ * Hudson Implementation of SVNEventAdapter
+ * @see org.tmatesoft.svn.core.wc.SVNEventAdapter
  *******************************************************************************/
+
 package org.eclipse.hudson.scm.subversion;
 
 import org.tmatesoft.svn.core.SVNErrorCode;
