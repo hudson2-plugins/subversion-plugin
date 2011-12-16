15943d91bbb44b87c6d8b4823afeb81e0043e03a
8nevil8 <nevil.nick@gmail.com>
2011-12-16 15:44:23 +0300

    Added XStream aliases for backward compatibility

--- src/main/java/org/eclipse/hudson/scm/subversion/PluginImpl.java
+++ src/main/java/org/eclipse/hudson/scm/subversion/PluginImpl.java
@@ -22,6 +22,13 @@ import hudson.model.Hudson;
 import java.io.IOException;
 import org.eclipse.hudson.scm.subversion.SubversionSCM.DescriptorImpl;
 import org.eclipse.hudson.scm.subversion.SubversionSCM.ModuleLocation;
+import org.eclipse.hudson.scm.subversion.browsers.CollabNetSVN;
+import org.eclipse.hudson.scm.subversion.browsers.FishEyeSVN;
+import org.eclipse.hudson.scm.subversion.browsers.Sventon;
+import org.eclipse.hudson.scm.subversion.browsers.Sventon2;
+import org.eclipse.hudson.scm.subversion.browsers.ViewSVN;
+import org.eclipse.hudson.scm.subversion.browsers.WebSVN;
+import org.eclipse.hudson.scm.subversion.browsers.WebSVN2;
 
 /**
  * Plugin entry point.
@@ -51,6 +58,13 @@ public class PluginImpl extends Plugin {
         Items.XSTREAM.alias("hudson.scm.SVNRevisionState", SVNRevisionState.class);
         Items.XSTREAM.alias("hudson.scm.SubversionSCM$ModuleLocation", ModuleLocation.class);
         Items.XSTREAM.alias("hudson.scm.SubversionTagAction", SubversionTagAction.class);
+        Items.XSTREAM.alias("hudson.scm.browsers.CollabNetSVN", CollabNetSVN.class);
+        Items.XSTREAM.alias("hudson.scm.browsers.FishEyeSVN", FishEyeSVN.class);
+        Items.XSTREAM.alias("hudson.scm.browsers.Sventon", Sventon.class);
+        Items.XSTREAM.alias("hudson.scm.browsers.Sventon2", Sventon2.class);
+        Items.XSTREAM.alias("hudson.scm.browsers.ViewSVN", ViewSVN.class);
+        Items.XSTREAM.alias("hudson.scm.browsers.WebSVN", WebSVN.class);
+        Items.XSTREAM.alias("hudson.scm.browsers.WebSVN2", WebSVN2.class);
 
         XmlFile.DEFAULT_XSTREAM.alias("hudson.scm.SubversionSCM$DescriptorImpl$PasswordCredential",
                 DescriptorImpl.PasswordCredential.class);
@@ -60,8 +74,13 @@ public class PluginImpl extends Plugin {
                 DescriptorImpl.SslClientCertificateCredential.class);
         XmlFile.DEFAULT_XSTREAM.alias("hudson.scm.SubversionSCM$DescriptorImpl", DescriptorImpl.class);
         XmlFile.DEFAULT_XSTREAM.alias("hudson.scm.SubversionTagAction", SubversionTagAction.class);
+        XmlFile.DEFAULT_XSTREAM.alias("hudson.scm.PerJobCredentialStore", PerJobCredentialStore.class);
 
         Run.XSTREAM.alias("hudson.scm.SubversionTagAction", SubversionTagAction.class);
+        Run.XSTREAM.alias("hudson.scm.SubversionSCM$SvnInfo", SubversionSCM.SvnInfo.class);
+        Run.XSTREAM.alias("hudson.scm.SubversionTagAction", SubversionTagAction.class);
+        Run.XSTREAM.alias("hudson.scm.SubversionChangeLogParser", SubversionChangeLogParser.class);
+        Run.XSTREAM.alias("hudson.scm.SVNRevisionState", SVNRevisionState.class);
 
         Hudson.XSTREAM.alias("hudson.scm.SubversionTagAction", SubversionTagAction.class);
     }
